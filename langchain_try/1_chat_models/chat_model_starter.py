4c7646a81165a3ee914d48a73eac0dec774a805c
ManvithGopu13 <manvithgopu1394@gmail.com>
2025-03-24 18:33:20 +0530

    Implement chat model with Google Generative AI and add .env file to .gitignore

--- langchain_try/1_chat_models/chat_model_starter.py
+++ langchain_try/1_chat_models/chat_model_starter.py
@@ -1,1 +1,14 @@
-print("Lets learn Langchain")
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+llm = ChatGoogleGenerativeAI(
+    model = "gemini-1.5-pro",
+    api_key = os.getenv("GEMINI_API_KEY")
+)
+
+result = llm.invoke("What is the capital of India?")
+
+print(result.content)
